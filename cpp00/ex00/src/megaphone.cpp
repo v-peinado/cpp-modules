bc7a02d2d8685a17d3df73cf239831ae5b3fb52a
v-peinado <peinado1985@gmail.com>
2024-03-19 19:57:05 +0100

    3

---
+++ cpp00/ex00/src/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpeinado <vpeinado@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 15:14:55 by vpeinado          #+#    #+#             */
+/*   Updated: 2024/03/19 19:44:53 by vpeinado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if(argc < 2)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        for(int i = 1; i < argc; i++)
+        {
+            for(int j = 0; argv[i][j]; j++)
+                std::cout << (char)toupper(argv[i][j]);
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+    return (0);
+}
