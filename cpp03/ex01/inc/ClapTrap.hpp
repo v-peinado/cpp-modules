9ff855c6156eaba5d95c576c3b720c603735f1b6
v-peinado <peinado1985@gmail.com>
2024-04-19 23:37:25 +0200

    holi

---
+++ cpp03/ex01/inc/ClapTrap.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpeinado <victor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 18:10:13 by vpeinado          #+#    #+#             */
+/*   Updated: 2024/04/19 22:17:41 by vpeinado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HHP
+#define CLAPTRAP_HHP
+
+#include <iostream>
+
+class ClapTrap
+{
+    private:
+        std::string _name;
+        int _hitpoints;
+        int _energyPoints;
+        int _attackDamage;
+    public:
+        ClapTrap(std::string name);
+        ~ClapTrap();
+        ClapTrap(const ClapTrap &other);
+        ClapTrap &operator=(const ClapTrap &rhs);
+
+        void attack(std::string const &target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+        void setAttackDamage(int damage);
+        int getAttackDamage();   
+};
+
+
+
+#endif
