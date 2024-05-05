6c896de2b73b6bdf4e143684434988a8439cefca
v-peinado <peinado1985@gmail.com>
2024-05-05 13:17:01 +0200

    use pointers

--- cpp03/ex00/src/main.cpp
+++ cpp03/ex00/src/main.cpp
@@ -6,22 +6,31 @@
 /*   By: vpeinado <victor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 18:10:10 by vpeinado          #+#    #+#             */
-/*   Updated: 2024/04/19 22:23:23 by vpeinado         ###   ########.fr       */
+/*   Updated: 2024/05/05 12:23:56 by vpeinado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 int main(void) {
-    ClapTrap a("Player1");
-    ClapTrap b("Player2");
-
-    a.attack("Player2");
-    b.takeDamage(a.getAttackDamage());
-    b.beRepaired(5);
-    a.setAttackDamage(5);
-    a.attack("Player2");
-    b.takeDamage(a.getAttackDamage());
-    b.beRepaired(10);
+    //ClapTrap a("Player1");
+    //ClapTrap b("Player2");
+    std::cout << "**CONTRUCTORS**" << std::endl;
+    ClapTrap *a = new ClapTrap("Player1");
+    ClapTrap *b = new ClapTrap("Player2");
+    std::cout << std::endl; 
+    
+    std::cout << "**ATTACK**" << std::endl;
+    a->attack("Player2");
+    b->takeDamage(a->getAttackDamage());
+    b->beRepaired(5);
+    a->setAttackDamage(5);
+    a->attack("Player2");
+    b->takeDamage(a->getAttackDamage());
+    b->beRepaired(10);
+    std::cout << std::endl;
+    std::cout << "**DESTRUCTORS**" << std::endl;
+    delete a;
+    delete b;
     return 0;
 }
