455dbdcd7857fc776407d2e47cd74d4682f4c50d
v-peinado <peinado1985@gmail.com>
2024-07-14 22:31:17 +0200

    main.cpp

--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vpeinado <victor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 21:06:05 by vpeinado          #+#    #+#             */
-/*   Updated: 2024/07/14 21:23:54 by vpeinado         ###   ########.fr       */
+/*   Updated: 2024/07/14 22:30:36 by vpeinado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,60 @@
 
 
 int main(void) {
+    
+    std::cout << "Se crea un array de 5 elementos" << std::endl;
+    Array<int> arr(5);
+    std::cout << "Se asignan valores aleatorios a los elementos" << std::endl;
+    for (unsigned int i = 0; i < arr.size(); i++)
+        arr[i] = rand();
+    std::cout << "Se imprime el array" << std::endl;
+    for (unsigned int i = 0; i < arr.size(); i++)
+        std::cout << arr[i] << std::endl;
+    std::cout << "Se crea otro array de 5 elementos" << std::endl;
+    Array<int> arr2(5);
+    std::cout << "Se asignan valores aleatorios a los elementos" << std::endl;
+    for (unsigned int i = 0; i < arr2.size(); i++)
+        arr2[i] = rand();
+    std::cout << "Se imprime el array" << std::endl;
+    for (unsigned int i = 0; i < arr2.size(); i++)
+        std::cout << arr2[i] << std::endl;
+    std::cout << "Se copia el primer array en el segundo" << std::endl;
+    arr2 = arr;
+    std::cout << "Se imprime el segundo array" << std::endl;
+    for (unsigned int i = 0; i < arr2.size(); i++)
+        std::cout << arr2[i] << std::endl;
+    std::cout << "Se modifica el segundo array" << std::endl;
+    for (unsigned int i = 0; i < arr2.size(); i++)
+        arr2[i] = 2;
+    std::cout << "Se imprime el primer array" << std::endl;
+    for (unsigned int i = 0; i < arr.size(); i++)
+        std::cout << arr[i] << std::endl;
+    std::cout << "Se imprime el segundo array para ver si son diferentes" << std::endl;
+    for (unsigned int i = 0; i < arr2.size(); i++)
+        std::cout << arr2[i] << std::endl;
+    std::cout << "Probamos crear un array vacio" << std::endl;
+    Array<int> arr3;
+    std::cout << "Se imprime el array vacio, no se imprime nada" << std::endl;
+    for (unsigned int i = 0; i < arr3.size(); i++)
+        std::cout << arr3[i] << std::endl;
+    std::cout << "Asignamos los valores de arr2 al elemento vacio" << std::endl;
+    arr3 = arr2;
+    std::cout << "Se imprime el arr3" << std::endl;
+    for (unsigned int i = 0; i < arr3.size(); i++)
+        std::cout << arr3[i] << std::endl;
+    std::cout << "Se intenta acceder a un elemento fuera de rango" << std::endl;
+    try {
+        arr2[10] = 0;
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "Se intenta acceder a un elemento fuera de rango" << std::endl;
+    try {
+        arr2[-1] = 0;
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    
     return 0;
 }
 
