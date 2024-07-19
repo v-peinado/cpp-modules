60497871b3521518b33497d8a4c60c06e2e41445
v-peinado <peinado1985@gmail.com>
2024-07-19 15:11:03 +0200

    more comments

--- cpp06/ex02/src/main.cpp
+++ cpp06/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vpeinado <victor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 13:00:26 by vpeinado          #+#    #+#             */
-/*   Updated: 2024/07/12 14:03:19 by vpeinado         ###   ########.fr       */
+/*   Updated: 2024/07/19 15:10:23 by vpeinado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,23 @@
 #include "C.hpp"
 /*
     dynamic_cast es un operador en C++ que se utiliza para convertir un puntero o una referencia
-    a un tipo de clase base a un puntero o referencia
+    de un tipo de clase base a un puntero o referencia
     a un tipo de clase derivada. Se usa en herencia y polimorfismo,
     permitiendo verificar y convertir dinámicamente tipos de objetos en tiempo de ejecución.
 
+    dynamic_cast puede usarse para castear un objeto a su propio tipo,
+    pero esto es innecesario en la mayoría de los casos, en este caso
+    nos es util para poder identificar el tipo de objeto que se esta manejando, ya que si se intenta castear
+    un puntero a un tipo que no es, el resultado sera NULL, por lo que se puede usar para identificar el tipo
+    de objeto que se esta manejando
+    En este caso nos es util para poder identificar el tipo de objeto que se esta manejando
     Porque es diferente la identificacion? por las propias caracteristicas de punteros y referencias:
     *Punteros: 
         La conversión con dynamic_cast devuelve nullptr si falla, permitiendo una simple verificación condicional.
     *Referencias: 
         La conversión con dynamic_cast lanza una excepción std::bad_cast si falla, requiriendo el uso de bloques try-catch.
+
+    
 */
 int main(void) {
     
